7f5da9767f65f74b1d4badebeed1e76c1f1e1596
eminkartci <amean.kartci@gmail.com>
2023-02-16 19:58:54 +0300

    dashboard tasarımı yapılıyor

---
+++ 16Subat2023Ders/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Taslağı</title>
+
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body style="display: flex;">
+    <!-- SIDEBAR BAŞLANGIÇ-->
+    <div class="sidebar" >
+        <!-- LOGO -->
+        <div class="logo-container"> logo </div>
+
+        <!-- Linkler -->
+        <div class="link-container"> 
+            <div class="link-item">
+                Link 1
+            </div>
+
+            <div class="link-item">
+                Link 2
+            </div>
+
+            <div class="link-item">
+                Link 3
+            </div>
+
+            <div class="link-item">
+                Link 4
+            </div>
+        </div>
+
+        <!-- Bildirimler -->
+        <div class="bildirim-container"> bildirim</div>
+
+        <!-- Firsatlar -->
+        <div class="firsat-container"> firsat </div>
+
+        <!-- Profil -->
+        <div class="profil-container"> profil</div>
+    </div>
+    <!-- SIDEBAR BİTİŞ-->
+
+
+    <!-- MAIN BAŞLANGIÇ-->
+    <div class="main-container">
+       <!-- Header -->
+       <div class="header-container">Header</div>
+
+       <!-- ICONS Section -->
+       <div class="icons-container">ICONS Section</div>
+
+       <!-- Main Content -->
+       <div class="mainsection-container">
+            <div class="chart-section1"></div>
+            <div class="chart-section1"></div>
+            <div class="chart-section2"></div>
+       </div>
+
+       <!-- Secondary content -->
+       <div class="secondarysection-container">Secondary content</div>
+
+       <!-- Footer -->
+       <div class="footer-container">Footer</div>
+    </div>
+    <!-- MAIN BİTİŞ-->
+</body>
+
+</html>
